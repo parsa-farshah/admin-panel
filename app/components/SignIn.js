3ac20296c15eb091af7623568c397d4e3a88ef3c
parsa <parsafarashah@gmail.com>
2025-12-19 17:56:05 +0330

    feat: add logIn

---
+++ app/components/SignIn.js
@@ -0,0 +1,242 @@
+"use client";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import {
+  Alert,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Typography,
+} from "@mui/material";
+import { useRouter } from "next/navigation";
+
+function SignIn() {
+  const [userName, setUserName] = React.useState("");
+  const [pass, setPass] = React.useState("");
+  const [flag, setFlag] = React.useState(0);
+  const router = useRouter();
+
+  const logInAlert = React.useRef();
+  const error = React.useRef();
+  const logIn = React.useRef();
+
+  const logInHandle = () => {
+    fetch("https://6912e51452a60f10c8232605.mockapi.io/adminpanel", {
+      method: "GET",
+      headers: { "content-type": "application/json" },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        // handle error
+      })
+      .then((tasks) => {
+        // Do something with the list of tasks
+        const userLogIN = tasks.some((val) => {
+          return val.username === userName && val.pass === pass;
+        });
+
+        if (userLogIN) {
+          logInAlert.current.style.left = "0%";
+          setTimeout(() => {
+            logInAlert.current.style.left = "-100%";
+          }, 1000);
+          setTimeout(() => {
+            router.push("dashboard");
+          }, 1500);
+        } else {
+          error.current.style.left = "0%";
+          setTimeout(() => {
+            error.current.style.left = "-100%";
+          }, 1500);
+          setUserName("");
+          setPass("");
+        }
+      })
+      .catch((error) => {
+        // handle error
+      });
+  };
+
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "100vh",
+        bgcolor: "#F5F4F9",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        overflow: "hidden",
+      }}
+    >
+      {/* error alert */}
+      <Alert
+        ref={error}
+        variant="filled"
+        severity="error"
+        sx={{
+          width: { xs: "90%", md: "30%" },
+          position: "absolute",
+          top: "5%",
+          left: "-100%",
+          zIndex: 50,
+          transition: "all 0.4s",
+        }}
+      >
+        Email or Password Wrong
+      </Alert>
+      {/* log in success alert */}
+
+      <Alert
+        variant="filled"
+        severity="success"
+        ref={logInAlert}
+        sx={{
+          width: { xs: "90%", md: "30%" },
+          position: "absolute",
+          top: "5%",
+          left: "-100%",
+          zIndex: 50,
+          transition: "all 0.4s",
+        }}
+      >
+        This is a filled success Alert.
+      </Alert>
+      <Box
+        sx={{
+          width: { xs: "95%", md: "30%", borderRadius: "24px" },
+          height: { xs: "450px", md: "400px" },
+          bgcolor: "black",
+          p: { xs: "7%", md: "2%" },
+        }}
+      >
+        <Typography variant="h5" sx={{ color: "white" }}>
+          Login
+        </Typography>
+        <Box sx={{ mt: "50px" }}>
+          {/*----------------------------------- userName input ------------*/}
+          <TextField
+            value={userName}
+            id="outlined-basic"
+            label="UserName"
+            variant="outlined"
+            sx={{
+              width: "100%",
+              color: "black",
+
+              "& label": {
+                color: "white",
+              },
+              "& label.Mui-focused": {
+                color: "white",
+              },
+
+              "& .MuiOutlinedInput-root": {
+                "& fieldset": {
+                  borderColor: "white",
+                },
+                "&:hover fieldset": {
+                  borderColor: "white",
+                },
+                "&.Mui-focused fieldset": {
+                  borderColor: "white",
+                },
+                "& .MuiInputBase-input": {
+                  color: "white",
+                },
+              },
+            }}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          {/*------------------------- password input -----------------------*/}
+          <TextField
+            value={pass}
+            id="outlined-password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            sx={{
+              width: "100%",
+              color: "black",
+              mt: { xs: "30px", md: "20px" },
+
+              "& label": {
+                color: "white",
+              },
+              "& label.Mui-focused": {
+                color: "white",
+              },
+
+              "& .MuiOutlinedInput-root": {
+                "& fieldset": {
+                  borderColor: "white",
+                },
+                "&:hover fieldset": {
+                  borderColor: "white",
+                },
+                "&.Mui-focused fieldset": {
+                  borderColor: "white",
+                },
+                "& .MuiInputBase-input": {
+                  color: "white",
+                },
+              },
+            }}
+            onChange={(e) => setPass(e.target.value)}
+          />
+          <Button
+            ref={logIn}
+            variant="outlined"
+            sx={{
+              width: "100%",
+              mt: { xs: "30px", md: "20px" },
+              py: "10px",
+              color: "white",
+              borderColor: "white",
+              "&:hover": {
+                borderColor: "white",
+                backgroundColor: "rgba(255,255,255,0.08)",
+              },
+            }}
+            onClick={logInHandle}
+          >
+            LogIn
+          </Button>
+          {/* auto complete */}
+          <FormControlLabel
+            onClick={() => {
+              setUserName("adminpanel");
+              setPass("adminpanel123");
+            }}
+            control={
+              <Checkbox
+                sx={{
+                  borderColor: "white",
+                  color: "white",
+
+                  "&.Mui-checked": {
+                    color: "white",
+                  },
+                  "&:hover": {
+                    backgroundColor: "rgba(255,255,255,0.08)",
+                  },
+                }}
+              />
+            }
+            label="Auto Fill"
+            sx={{
+              color: "white",
+              borderColor: "white",
+              mt: { xs: "30px", md: "20px" },
+            }}
+          />
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default SignIn;
--- app/page.js
+++ app/page.js
@@ -17,6 +17,7 @@ import { use, useEffect, useState } from "react";
 import Header from "./components/Header";
 import { Close, DragHandle } from "@mui/icons-material";
 import Main from "./components/Main";
+import SignIn from "./components/SignIn";
 
 export default function Home() {
   const [theme, setTheme] = useState(true);
@@ -42,91 +43,7 @@ export default function Home() {
   };
   return (
     <myTheme.Provider value={{ theme, lang }}>
-      <Stack
-        className={lang ? urbanist.className : alexandria.className}
-        direction={"row"}
-        sx={{
-          bgcolor: theme ? "#F5F4F9" : "black",
-          px: "17px",
-          py: 4,
-          height: "100vh",
-          justifyContent: "space-between",
-          direction: lang ? "ltr" : "rtl",
-          transition: "all 0.4s",
-          position: "relative",
-          overflowX: "hidden",
-          width: { xs: "100%", xl: 1536 },
-          mx: { xl: "auto" },
-        }}
-      >
-        {/*---------------------------------- sidebar ------------------------*/}
-        {menuMobile ? (
-          <Box
-            sx={{
-              width: "100%",
-              height: "100%",
-              display: "flex",
-              position: "fixed",
-              display: { xs: { dispaly }, md: "none" },
-              top: 0,
-              left: 0,
-              p: 1,
-              zIndex: 50,
-            }}
-          >
-            <Sidebar />
-            {/* close menu */}
-            <Box
-              sx={{
-                position: "absolute",
-                zIndex: 51,
-                right: lang ? "5%" : "85%",
-                top: "5%",
-              }}
-            >
-              <Fab
-                onClick={menuMobileBtn}
-                sx={{
-                  bgcolor: theme ? "#EBEAEF" : "#323233",
-                  color: "#8E8D93",
-                  width: 40,
-                  height: 40,
-                  fontSize: "18px",
-                  boxShadow: "1px 1px 5px #1b1b1b67",
-                  display: { xs: { dispaly }, md: "none" },
-                  transition: "all 0.4s",
-                  "&:hover": {
-                    bgcolor: theme ? "#ebeaefb6" : "#323233c9",
-                  },
-                }}
-              >
-                <Close />
-              </Fab>
-            </Box>
-          </Box>
-        ) : (
-          <Box
-            sx={{
-              width: "22%",
-              height: "100%",
-              display: { xs: "none", md: "block" },
-            }}
-          >
-            <Sidebar />
-          </Box>
-        )}
-        {/*----------------------------------- main -----------------------------*/}
-        <Box sx={{ width: { xs: "100%", md: "77%" }, height: "100%" }}>
-          <Header
-            themeChange={themeChange}
-            changelangeEn={changelangeEn}
-            changelangeFa={changelangeFa}
-            menuMobileBtn={menuMobileBtn}
-            dispaly={dispaly}
-          />
-          <Main />
-        </Box>
-      </Stack>
+      <SignIn />
     </myTheme.Provider>
   );
 }
